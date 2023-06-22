018618d581d32c8e65e167b3bbc8baec542bd3d6
AdilTehranli <adiljt@code.edu.az>
2023-06-22 23:28:09 +0400

    Updated Dashboard

--- Dashboard.js
+++ Dashboard.js
@@ -1,60 +1,103 @@
-let form=document.querySelector('form');
+let form = document.querySelector("form");
 
-form.addEventListener('submit',async (e)=>{
-    e.preventDefault()
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    let title=document.querySelector('#title').value
-    let price=document.querySelector('#price').value
-    let description=document.querySelector('#description').value    
+  let title = document.querySelector("#title").value;
+  let price = document.querySelector("#price").value;
+  let description = document.querySelector("#description").value;
 
-    const response= await fetch(`https://source.unsplash.com/random/900×700/?${title}`)
+  const response = await fetch(
+    `https://source.unsplash.com/random/900×700/?${title}`
+  );
 
-    let products={
-        title,
-        price,
-        description,
-        image:response.url
+  let products = {
+    title,
+    price,
+    description,
+    image: response.url,
+  };
+
+  const result = await fetch(
+    `https://loginjs-ec3df-default-rtdb.firebaseio.com/products.json`,
+    {
+      method: "POST",
+      body: JSON.stringify(products),
     }
+  );
 
-    const result=await fetch(`https://loginjs-ec3df-default-rtdb.firebaseio.com/products.json`,{
-        method: 'POST',
-        body:JSON.stringify(products)
-    })
-})
+  getData();
+});
 
-onload=getData;
-async   function getData(){
-    let response= await fetch(`https://loginjs-ec3df-default-rtdb.firebaseio.com/products.json`);
+onload = getData;
+async function getData() {
+  let response = await fetch(
+    `https://loginjs-ec3df-default-rtdb.firebaseio.com/products.json`
+  );
 
-    let data=await response.json()    
-    
-    let products=[];
+  let data = await response.json();
 
-    for(let key  in data){
-      
-        data[key].id=key
-       products.push(data[key])
-    }
-    let tr=''
-    products.forEach((item,index)=>{
-        tr+=`
-        <tr>
-            <th scope="row">${index++}</th>
-            <td>${item.title}</td>
-            <td><img src="${item.image}"></td>
-            <td>${item.price}$</td>
-            <td><button onclick="Delete('${item.id}')">Delete</button></td>
+  let products = [];
 
-          </tr>
-        `
-        document.querySelector('tbody').innerHTML=tr;
-    })
-}   
+  for (let key in data) {
+    data[key].id = key;
+    products.push(data[key]);
+  }
+
+  let tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  products.forEach((item, index) => {
+    let tr = document.createElement("tr");
+
+    let indexCell = document.createElement("th");
+    indexCell.setAttribute("scope", "row");
+    indexCell.textContent = index + 1;
+    tr.appendChild(indexCell);
+
+    let titleCell = document.createElement("td");
+    titleCell.textContent = item.title;
+    tr.appendChild(titleCell);
+
+    let imageCell = document.createElement("td");
+    let image = document.createElement("img");
+    image.setAttribute("src", item.image);
+    imageCell.appendChild(image);
+    tr.appendChild(imageCell);
+
+    let priceCell = document.createElement("td");
+    priceCell.textContent = item.price + "$";
+    tr.appendChild(priceCell);
+
+    let deleteCell = document.createElement("td");
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+      Delete(item.id);
+    });
+    deleteCell.appendChild(deleteButton);
+    tr.appendChild(deleteCell);
+
+    let updateCell = document.createElement("td");
+    let updateButton = document.createElement("button");
+    updateButton.textContent = "Update";
+    updateButton.addEventListener("click", () => {
+      Update(item.id);
+    });
+    updateCell.appendChild(updateButton);
+    tr.appendChild(updateCell);
+
+    tbody.appendChild(tr);
+  });
+}
 
 async function Delete(id) {
-    let response = await fetch(`https://loginjs-ec3df-default-rtdb.firebaseio.com/products/${id}.json`, {
-      method: 'DELETE'
-    });
-   
-  }
-  
+  let response = await fetch(
+    `https://loginjs-ec3df-default-rtdb.firebaseio.com/products/${id}.json`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  getData();
+}
